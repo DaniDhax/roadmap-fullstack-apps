1324e9cd5d1bba45b0aa272d7abe1b4f90fd7a00
DaniDhax <111738016+DaniDhax@users.noreply.github.com>
2024-03-08 12:21:16 +0100

    Ejercicio 4 calculadora terminado (un poco meh, pero suficiente)

--- e1-figma-clone/tailwind.config.js
+++ e1-figma-clone/tailwind.config.js
@@ -1,23 +1,24 @@
 /** @type {import('tailwindcss').Config} */
-const plugin = require('tailwindcss/plugin')
+import plugin from 'tailwindcss/plugin';
 
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {
-      colors: {
-        transparent: "transparent",
-        current: "currentColor",
-        white: "hsl(0, 0%, 100%)",
-        "body-background": "#252930",
-      },
-      fontFamily: {
-        "recursive": ['"Recursive"', "sans-serif"],
-        "poppins": ['"Poppins"', "sans-serif"],
-        "Source-Sans-3": ['"Source Sans 3"', "sans-serif"],
-      },
-      
+export const content = ["./src/**/*.{html,js}"];
+export const theme = {
+  extend: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "hsl(0, 0%, 100%)",
+      "shark": "#252930",
+      "bittersweet": "#FF7575",
+      "tundora": "#4D4D4D",
+      "gray": "#909090",
+      "havelock-blue": "#4699E0",
+    },
+    fontFamily: {
+      "recursive": ['"Recursive"', "sans-serif"],
+      "poppins": ['"Poppins"', "sans-serif"],
+      "Source-Sans-3": ['"Source Sans 3"', "sans-serif"],
     },
   },
-  plugins: [],
 };
+export const plugins = [];
--- e4-calculadora-vite-ts-tailwind/src/main.ts
+++ e4-calculadora-vite-ts-tailwind/src/main.ts
@@ -1,6 +1,7 @@
 import './style.css'
 
 let varAcumulador: number = 0
+let varOperador: string = ""
 
 const txtResultado: HTMLElement | null = document.getElementById('txtResultado')
 
@@ -13,6 +14,8 @@ btnC?.addEventListener('click', limpiarTxtResultado)
 function limpiarTxtResultado() {
   if (txtResultado) {
     txtResultado.innerText = "0";
+    varAcumulador = 0
+    varOperador = ""
   }
 }
 
@@ -22,9 +25,9 @@ btnRetroceso?.addEventListener('click', () => {
   if (!txtResultado) {
     return;
   }
-  
+
   let resultado: string = txtResultado.innerText;
-  
+
   // Parsear el resultado solamente si es un número válido
   const numeroParseado: number = parseInt(resultado);
   if (!isNaN(numeroParseado)) {
@@ -32,7 +35,7 @@ btnRetroceso?.addEventListener('click', () => {
     resultado = resultado.slice(0, -1);
     txtResultado.innerText = resultado;
   }
-  
+
   if (txtResultado.innerText === '') {
     txtResultado.innerText = '0'
   }
@@ -44,79 +47,107 @@ btnRetroceso?.addEventListener('click', () => {
 //! Botones de los números y función para modificar la ventana de los resultados -------------------
 const btnNum0: HTMLElement | null = document.getElementById('btnNum0')
 btnNum0?.addEventListener('click', () => {
-  if (txtResultado == null) {return} 
+  if (txtResultado == null) { return }
   modificarTxtResultado('0')
 })
 
 const btnNum1: HTMLElement | null = document.getElementById('btnNum1')
 btnNum1?.addEventListener('click', () => {
-  if (txtResultado == null) {return} 
+  if (txtResultado == null) { return }
   modificarTxtResultado('1')
 })
 
 const btnNum2: HTMLElement | null = document.getElementById('btnNum2')
 btnNum2?.addEventListener('click', () => {
-  if (txtResultado == null) {return} 
+  if (txtResultado == null) { return }
   modificarTxtResultado('2')
 })
 
 const btnNum3: HTMLElement | null = document.getElementById('btnNum3')
 btnNum3?.addEventListener('click', () => {
-  if (txtResultado == null) {return} 
+  if (txtResultado == null) { return }
   modificarTxtResultado('3')
 })
 
 const btnNum4: HTMLElement | null = document.getElementById('btnNum4')
 btnNum4?.addEventListener('click', () => {
-  if (txtResultado == null) {return} 
+  if (txtResultado == null) { return }
   modificarTxtResultado('4')
 })
 
 const btnNum5: HTMLElement | null = document.getElementById('btnNum5')
 btnNum5?.addEventListener('click', () => {
-  if (txtResultado == null) {return} 
+  if (txtResultado == null) { return }
   modificarTxtResultado('5')
 })
 
 const btnNum6: HTMLElement | null = document.getElementById('btnNum6')
 btnNum6?.addEventListener('click', () => {
-  if (txtResultado == null) {return} 
+  if (txtResultado == null) { return }
   modificarTxtResultado('6')
 })
 
 const btnNum7: HTMLElement | null = document.getElementById('btnNum7')
 btnNum7?.addEventListener('click', () => {
-  if (txtResultado == null) {return} 
+  if (txtResultado == null) { return }
   modificarTxtResultado('7')
 })
 
 const btnNum8: HTMLElement | null = document.getElementById('btnNum8')
 btnNum8?.addEventListener('click', () => {
-  if (txtResultado == null) {return} 
+  if (txtResultado == null) { return }
   modificarTxtResultado('8')
 })
 
 const btnNum9: HTMLElement | null = document.getElementById('btnNum9')
 btnNum9?.addEventListener('click', () => {
-  if (txtResultado == null) {return} 
+  if (txtResultado == null) { return }
   modificarTxtResultado('9')
 })
 
-function modificarTxtResultado (numero: string) {
-  if (txtResultado == null) {return} 
-  if (txtResultado?.innerText !== '0') {
-    txtResultado.innerText += numero
-  } else {
-    txtResultado.innerText = numero
+function modificarTxtResultado(numero: string) {
+  if (txtResultado == null) { return }
+
+  if (varOperador === "") {
+    if (txtResultado?.innerText !== '0') {
+      txtResultado.innerText += numero
+    } else {
+      txtResultado.innerText = numero
+    }
+  }  else {
+
+    if (varOperador === "sumar") {
+      varAcumulador = varAcumulador + parseInt(numero)
+      txtResultado.innerText = varAcumulador.toString()
+    }
+
+    if (varOperador === "restar") {
+      varAcumulador = varAcumulador - parseInt(numero)
+      txtResultado.innerText = varAcumulador.toString()
+    }
+
+    if (varOperador === "multiplicar") {
+      varAcumulador = varAcumulador * parseInt(numero)
+      txtResultado.innerText = varAcumulador.toString()
+    }
+    
+    if (varOperador === "dividir") {
+      varAcumulador = varAcumulador / parseInt(numero)
+      txtResultado.innerText = varAcumulador.toString()
+    }
+
   }
+
 }
+
 //! --------------------------------------------------------------------------------------
+
 
 const btnComa: HTMLElement | null = document.getElementById('btnComa')
 btnComa?.addEventListener('click', () => {
-  if (txtResultado == null) {return} 
-  if (txtResultado?.innerText.indexOf(".") !== -1) {return} //* Si ya se había utilizado la coma (punto) anteriormente.
-  
+  if (txtResultado == null) { return }
+  if (txtResultado?.innerText.indexOf(".") !== -1) { return } //* Si ya se había utilizado la coma (punto) anteriormente.
+
   if (txtResultado?.innerText !== '0') {
     txtResultado.innerText += "."
   } else {
@@ -130,29 +161,44 @@ btnComa?.addEventListener('click', () => {
 //! Operaciones aritméticas:
 const btnSumar: HTMLElement | null = document.getElementById('btnSumar')
 btnSumar?.addEventListener('click', () => {
-  if (txtResultado == null) {return} 
-  varAcumulador = parseInt(txtResultado?.innerText)
-  console.log(typeof(varAcumulador), varAcumulador)
-})
+  if (txtResultado == null) { return }
 
-const btnDividir: HTMLElement | null = document.getElementById('btnDividir')
-btnDividir?.addEventListener('click', () => {
-  console.log('btnDividir')
-})
+  varOperador = "sumar"
+  varAcumulador = parseInt(txtResultado.innerText)
 
-const btnMultiplicar: HTMLElement | null = document.getElementById('btnMultiplicar')
-btnMultiplicar?.addEventListener('click', () => {
-  console.log('btnMultiplicar')
 })
 
 const btnRestar: HTMLElement | null = document.getElementById('btnRestar')
 btnRestar?.addEventListener('click', () => {
-  console.log('btnRestar')
+  if (txtResultado == null) { return }
+
+  varOperador = "restar"
+  varAcumulador = parseInt(txtResultado.innerText)
+
+})
+
+const btnMultiplicar: HTMLElement | null = document.getElementById('btnMultiplicar')
+btnMultiplicar?.addEventListener('click', () => {
+  if (txtResultado == null) { return }
+
+  varOperador = "multiplicar"
+  varAcumulador = parseInt(txtResultado.innerText)
+
+})
+
+const btnDividir: HTMLElement | null = document.getElementById('btnDividir')
+btnDividir?.addEventListener('click', () => {
+  if (txtResultado == null) { return }
+
+  varOperador = "dividir"
+  varAcumulador = parseInt(txtResultado.innerText)
 })
 
 
 
-const btnIgual: HTMLElement | null = document.getElementById('btnIgual')
-btnIgual?.addEventListener('click', () => {
-  console.log('')
-})
+
+
+// const btnIgual: HTMLElement | null = document.getElementById('btnIgual')
+// btnIgual?.addEventListener('click', () => {
+//   console.log('')
+// })
