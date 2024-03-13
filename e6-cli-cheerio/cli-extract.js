c7606d3a8dfb6397d2bb991a0130ee809244fdde
DaniDhax <111738016+DaniDhax@users.noreply.github.com>
2024-03-13 14:40:48 +0100

    Añadido ejercicio 6

---
+++ e6-cli-cheerio/cli-extract.js
@@ -0,0 +1,14 @@
+const readline = require('readline')
+
+// const rl = readline.createInterface(process.stdin, process.stdout)
+// rl.question("Introduce algo ", function(respuesta) {
+//     console.log('Has escrito: ', respuesta)
+//     rl.close()
+// })
+
+const rl = readline.createInterface(process.stdin, process.stdout)
+rl.question("Introduce algo ", function(respuesta) {
+    console.log('Has escrito: ', respuesta)
+    rl.close()
+})
+
